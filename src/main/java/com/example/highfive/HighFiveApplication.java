028f8c7b0e1a148066ab1c2e7e74755a2c505121
semi-cloud <sksmsrkdtpal@naver.com>
2022-07-05 22:49:47 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/example/highfive/HighFiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.highfive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighFiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HighFiveApplication.class, args);
+    }
+
+}
