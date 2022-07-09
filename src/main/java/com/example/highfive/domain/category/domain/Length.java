95728c0cfa41471a165da7807be2cc620abbe7bb
Semi kang <71436576+semi-cloud@users.noreply.github.com>
2022-07-09 22:39:42 +0900

    :sparkles: JPA 매핑 추가 (#6) (#7)

---
+++ src/main/java/com/example/highfive/domain/category/domain/Length.java
@@ -0,0 +1,5 @@
+package com.example.highfive.domain.category.domain;
+
+public enum Length {
+    짧음, 보통, 긺
+}
---
+++ src/main/java/com/example/highfive/domain/category/domain/Thick.java
@@ -0,0 +1,21 @@
+package com.example.highfive.domain.category.domain;
+
+public enum Thick {
+
+    매우_얇음(1.2, 0.0, 0.0),
+    얇음(1.1,0.5, 1.0),  // 옷, 신발, 모자
+    보통(1.0, 0.0, 0.0),
+    두꺼움(0.9, 1.0, 2.0),
+    매우_두꺼움(0.8, 0.0 ,0.0);
+
+    private final double percent;
+    private final double second;
+
+    private final double third;
+
+    Thick(double percent, double second, double third){
+        this.percent = percent;
+        this.second = second;
+        this.third = third;
+    }
+}
